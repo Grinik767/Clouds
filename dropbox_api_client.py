0917c79f9344efae66f0345b9455b021d8d1420d
Cyeciad <loginmish@gmail.com>
2024-05-17 18:40:56 +0500

    error worker fixes

--- dropbox_api_client.py
+++ dropbox_api_client.py
@@ -20,7 +20,7 @@ class Dropbox(Cloud):
             self.session.headers = {"Authorization": auth_token}
             return
         self.error_worker(
-            {"error": {".tag": "AuthError"}, "message": "Ошибка авторизации в Dropbox. Проверьте/обновите данные"})
+            {"error": {".tag": "AuthError"}, "error_summary": "Ошибка авторизации в Dropbox. Проверьте/обновите данные"})
 
     def get_cloud_info(self) -> dict:
         r = self.session.post(f"{self.url}users/get_space_usage",
@@ -42,20 +42,21 @@ class Dropbox(Cloud):
     def get_folder_content(self, path: str) -> dict:
         headers = {"Authorization": f"Bearer {self.auth_token}",
                    "Content-Type": "application/json"}
-        r = self.session.post(f"{self.url}/files/get_metadata", headers=headers,
+        r = self.session.post(f"{self.url}files/get_metadata", headers=headers,
                               json={"include_deleted": False, "include_has_explicit_shared_members": False,
                                     "include_media_info": False, "path": path
                                     })
-        if r.status_code != 200:
+        if r.status_code != httpx.codes.OK:
             return self.error_worker(r.json())
         if r.json()['.tag'] != "folder":
-            return self.error_worker({"error": "NotAFolder", "message": "Запрошенный ресурс не является папкой"})
+            return self.error_worker(
+                {"error": {".tag": "NotAFolder"}, "error_summary": "Запрошенный ресурс не является папкой"})
 
         r = self.session.post(f"{self.url}files/list_folder",
                               json={"path": path, "recursive": False, "include_media_info": False,
                                     "include_deleted": False, "include_has_explicit_shared_members": False
                                     }, headers=headers)
-        if r.status_code != 200:
+        if r.status_code != httpx.codes.OK:
             return self.error_worker(r.json())
 
         content_info = r.json()
@@ -82,8 +83,8 @@ class Dropbox(Cloud):
             return {"status": "ok"}
         except FileNotFoundError:
             return self.error_worker({
-                "error": "FileNotFoundError",
-                "message": f"Неверный путь: {path.abspath(path_local)}"
+                "error": {".tag": "FileNotFoundError"},
+                "error_summary": f"Неверный путь: {path.abspath(path_local)}"
             })
 
     def upload_file(self, path_local: str, path_remote: str) -> dict:
@@ -102,10 +103,12 @@ class Dropbox(Cloud):
                     "Content-Type": "application/octet-stream"
                 }
                 r = self.session.post("https://content.dropboxapi.com/2/files/upload", headers=headers, files=files)
+                if r.status_code != 200:
+                    return self.error_worker(r.json())
         except FileNotFoundError:
             return self.error_worker({
-                "error": "FileNotFoundError",
-                "message": f"Файл не найден: {path_local}"
+                "error": {".tag": "FileNotFoundError"},
+                "error_summary": f"Файл не найден: {path_local}"
             })
 
         if r.status_code == 200:
@@ -120,16 +123,17 @@ class Dropbox(Cloud):
             "Content-Type": "application/json"
         }
         r = self.session.post("https://api.dropboxapi.com/2/files/create_folder_v2", json=data, headers=headers)
+        print(r.status_code)
         if r.status_code == 200:
             return {"status": "ok"}
         else:
             return self.error_worker(r.json())
+
     @staticmethod
     def error_worker(response: dict):
         with SystemClass.except_handler(SystemClass.exchandler):
-            print(response)
-            raise Exception(f"{response['error']['.tag']}. {response['message']}")
+            raise Exception(f"{response['error']['.tag']}. {response['error_summary']}")
 
 
 if __name__ == '__main__':
-    pass
+    driver = Dropbox("")
