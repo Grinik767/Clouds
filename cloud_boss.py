1b2ec3344f929c5b1c73d2347444f1337c0e71b7
Grinik767 <grigorikiseleff@yandex.ru>
2024-05-23 14:12:20 +0500

    few fixes

--- cloud_boss.py
+++ cloud_boss.py
@@ -22,31 +22,41 @@ class CloudBoss:
         except httpx.HTTPError:
             click.echo("Произошла ошибка. Попробуйте позже.")
 
-    async def get_folder_content(self, cloud_name: str, path: str):
+    async def get_folder_content(self, cloud_name: str, path_remote: str):
         try:
-            response = await self.clouds[cloud_name].get_folder_content(path)
+            response = await self.clouds[cloud_name].get_folder_content(path_remote)
             click.echo('\n'.join(map(lambda el: f"/{el}", response['folders'])))
             click.echo('\n'.join(response['files']))
         except httpx.HTTPError:
             click.echo("Произошла ошибка. Попробуйте позже.")
 
-    def create_folder(self, cloud_name: str, path: str):
+    async def create_folder(self, cloud_name: str, path_remote: str):
         try:
-            self.clouds[cloud_name].create_folder(path)
+            await self.clouds[cloud_name].create_folder(path_remote)
             click.echo("Папка успешно создана!")
         except httpx.HTTPError:
             click.echo("Произошла ошибка. Попробуйте позже.")
 
-    def download(self, cloud_name: str, path_remote: str, path_local: str):
+    async def download(self, cloud_name: str, path_remote: str, path_local: str):
         try:
-            self.clouds[cloud_name].download_file(path_remote, path_local)
-            click.echo("Файл успешно скачан!")
-        except httpx.HTTPError:
-            click.echo("Произошла ошибка. Попробуйте позже.")
+            await self.clouds[cloud_name].download_folder(path_remote, path_local)
+            click.echo("Папка успешно скачана!")
+        except Exception as e:
+            SystemClass.exchandler(type(e), e, e.__traceback__)
+            try:
+                await self.clouds[cloud_name].download_file(path_remote, path_local)
+                click.echo("Файл успешно скачан!")
+            except Exception as e:
+                SystemClass.exchandler(type(e), e, e.__traceback__)
 
-    def upload(self, cloud_name: str, path_local: str, path_remote: str):
+    async def upload(self, cloud_name: str, path_local: str, path_remote: str):
         try:
-            self.clouds[cloud_name].upload_file(path_local, path_remote)
-            click.echo("Файл успешно загружен!")
-        except httpx.HTTPError:
-            click.echo("Произошла ошибка. Попробуйте позже.")
+            await self.clouds[cloud_name].upload_folder(path_local, path_remote)
+            click.echo("Папка успешно загружена!")
+        except Exception as e:
+            SystemClass.exchandler(type(e), e, e.__traceback__)
+            try:
+                await self.clouds[cloud_name].upload_file(path_local, path_remote)
+                click.echo("Файл успешно загружен!")
+            except Exception as e:
+                SystemClass.exchandler(type(e), e, e.__traceback__)
