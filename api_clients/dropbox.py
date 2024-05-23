cbfb5c7b79c04c4a149b13db5e7092f0b6e2189b
Cyeciad <loginmish@gmail.com>
2024-05-23 13:03:09 +0500

    code style fixes

--- api_clients/dropbox.py
+++ api_clients/dropbox.py
@@ -76,51 +76,56 @@ class Dropbox(Cloud):
                        isinstance(entry, dict) and entry[".tag"] == "folder"]
             return {"folders": folders, "files": files}
 
-    async def download_file(self, path_remote: str, path_local: str) -> dict:
+    async def download(self, path_remote: str, is_file: bool = True) -> bytes:
         async with httpx.AsyncClient(headers=self.headers) as session:
+            headers = {"Authorization": f"Bearer {self.auth_token}",
+                       "Content-Type": "application/json"}
+            r = await session.post(f"{self.url}files/get_metadata", headers=headers,
+                                   json={"include_deleted": False, "include_has_explicit_shared_members": False,
+                                         "include_media_info": False, "path": f"{path_remote}"
+                                         })
+            if r.status_code != 200:
+                return self.error_worker(
+                    {"error": {".tag": "NotFoundError"}, "error_summary": "Не удалось найти запрошенный ресурс."})
+            resp = r.json()
+            if resp['.tag'] == "folder" and is_file:
+                return self.error_worker(
+                    {"error": {".tag": "NotAFileError"}, "error_summary": "Запрошенный ресурс не является файлом"})
+            elif resp['.tag'] != "folder" and not is_file:
+                return self.error_worker(
+                    {"error": {".tag": "NotAFolderError"}, "error_summary": "Запрошенный ресурс не является папкой"})
             dropbox_api_arg = json.dumps({"path": f"{path_remote}"})
 
             headers = {
                 "Authorization": f"Bearer {self.auth_token}",
                 "Dropbox-API-Arg": dropbox_api_arg
             }
-            r = await session.post(f"https://content.dropboxapi.com/2/files/download", headers=headers)
+            r = None
+            if is_file:
+                r = await session.post(f"https://content.dropboxapi.com/2/files/download", headers=headers)
+            else:
+                r = await session.post(f"https://content.dropboxapi.com/2/files/download_zip", headers=headers)
             if r.status_code != 200:
-                if r.status_code == 404:
-                    return self.error_worker(
-                        {"error": {".tag": "NotFoundError"}, "error_summary": "Не удалось найти запрошенный ресурс."})
                 return self.add_error(r)
-            try:
-                with open(path.abspath(path_local), 'wb') as file:
-                    file.write(r.content)
-                return {"status": "ok"}
-            except FileNotFoundError:
-                return self.error_worker({
-                    "error": {".tag": "FileNotFoundError"},
-                    "error_summary": f"Неверный путь: {path.abspath(path_local)}"
-                })
+            return r.content
+
+    async def download_file(self, path_remote: str, path_local: str) -> dict:
+        if path.isdir(path.abspath(path_local)):
+            return self.error_worker(
+                {"error": "FileNotFoundError", "message": f"Неверный путь: {path.abspath(path_local)}"})
+        await self.save_file(path_local, await self.download(path_remote))
+        return {"status": "ok"}
 
     async def download_folder(self, path_remote: str, path_local: str) -> dict:
-        async with httpx.AsyncClient(headers=self.headers) as session:
-            dropbox_api_arg = json.dumps({"path": f"{path_remote}"})
-
-            headers = {
-                "Authorization": f"Bearer {self.auth_token}",
-                "Dropbox-API-Arg": dropbox_api_arg
-            }
-            r = await session.post(f"https://content.dropboxapi.com/2/files/download_zip", headers=headers)
-            if r.status_code != 200:
-                if r.status_code == 404:
-                    return self.error_worker(
-                        {"error": {".tag": "NotFoundError"}, "error_summary": "Не удалось найти запрошенный ресурс."})
-                return self.add_error(r)
-
-            path_to_zip = path.join(path.abspath(path_local), "archive.zip")
-            await self.save_file(path_to_zip, r.content)
-            with zipfile.ZipFile(path_to_zip) as zip_ref:
-                zip_ref.extractall(path_local)
-            os.remove(path_to_zip)
-            return {"status": "ok"}
+        if path.isfile(path.abspath(path_local)):
+            return self.error_worker(
+                {"error": "FolderNotFoundError", "message": f"Неверный путь: {path.abspath(path_local)}"})
+        path_to_zip = path.join(path.abspath(path_local), "archive.zip")
+        await self.save_file(path_to_zip, await self.download(path_remote, is_file=False))
+        with zipfile.ZipFile(path_to_zip) as zip_ref:
+            zip_ref.extractall(path_local)
+        os.remove(path_to_zip)
+        return {"status": "ok"}
 
     async def save_file(self, path_local: str, content: bytes):
         try:
